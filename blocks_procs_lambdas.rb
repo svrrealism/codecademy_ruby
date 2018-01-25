85ec85b7ae7644ab8b26468bccf841f46269dfa5
svrrealism <svrrealists@gmail.com>
2018-01-25 09:04:58 -0500

    Define yield method and add Procs

--- blocks_procs_lambdas.rb
+++ blocks_procs_lambdas.rb
@@ -25,4 +25,24 @@ yield_name("eric") { |n| puts "My name is #{n}"}
 yield_name("zenulv") { |n| puts "My name is #{n}"}
 
 
-#defining our own yield method 
+#defining our own yield method
+def double(n)
+  yield(n)
+end
+
+double(2) { |i| i*2 }
+
+
+#So basically I imagine some code happening and then you can just pass off the
+# current result of where it's at to some block that can be outside of the method's
+# internal logic. Almost like planning the orchestration and then being able to change
+# the values on the fly. Useful.
+
+# Save a block using something called Procs
+# The following prints out multiples of 3 by passing a range converted to an iterable array
+# to then be passed to a block
+multiples_of_3 = Proc.new do |n|
+  n%3 == 0
+end
+
+puts (1..100).to_a.select(&multiples_of_3)
