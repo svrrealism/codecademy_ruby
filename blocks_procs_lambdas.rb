f83e07096ef1302fb20e6976e14d39fcfad8142b
svrrealism <svrrealists@gmail.com>
2018-01-24 22:46:13 -0500

    added blocks

--- blocks_procs_lambdas.rb
+++ blocks_procs_lambdas.rb
@@ -8,3 +8,21 @@ def block_test
 end
 
 block_test { puts "Now...in the block... " }
+
+
+
+#Can also yield with parameters (pass parameters to yield)
+
+def yield_name(name)
+  puts "I'm in the method, let's yield"
+  yield("Kim")
+  puts "In between the yields"
+  yield(name)
+  puts "Block complete, back in the method"
+end
+
+yield_name("eric") { |n| puts "My name is #{n}"}
+yield_name("zenulv") { |n| puts "My name is #{n}"}
+
+
+#defining our own yield method 
