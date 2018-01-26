1a549330b4300392aa4232a60e80b3e60effad69
svrrealism <svrrealists@gmail.com>
2018-01-26 18:50:52 -0500

    Complete OOP section 1

--- oop_p1.rb
+++ oop_p1.rb
@@ -1,10 +1,18 @@
 #Create a person class
 #initialize 'boots up' every object of the instanced class
-
+#determine the number of instances a class has by tracking it with a class var
 class Person
+  @@people_count = 0
   def initialize(name)
     @name = name
+    @@people_count += 1
+  end
+  def self.count_people
+    return @@people_count
   end
 end
 
 matz = Person.new("Yukihiro")
+user1 = Person.new("zenulv")
+
+puts Person.count_people
